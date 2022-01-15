f80cfc57767d9e43e4e3ed47d2683a228d63920c
Guuhp <lg645471@gmail.com>
2022-01-15 02:32:00 -0400

    Domain model, database seed

---
+++ backend/src/main/java/com/guuhp/dsmovie/Entities/Movie.java
@@ -0,0 +1,72 @@
+package com.guuhp.dsmovie.Entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tb_movie")
+public class Movie {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private Double score;
+    private int count;
+    private String image;
+    
+    public Movie(){
+
+    }
+
+    public Movie(long id, String title, Double score, int count, String image) {
+        this.id = id;
+        this.title = title;
+        this.score = score;
+        this.count = count;
+        this.image = image;
+    }
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Double getScore() {
+        return this.score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getImage() {
+        return this.image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+}
